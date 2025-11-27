2a6e98362bf6c2630b697f35f53fc2cccb4b7401
danielmarcosferreira <daniel.marcos.1038@gmail.com>
2025-11-26 23:18:12 -0300

    feat(auth): implement JWT authentication with bcrypt and update signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SignInDTO, SignUpDTO } from './dtos/auth';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable() // Injectable diz pro nest the as classes subsequentes serao providers 
 export class AuthService {
-    constructor(private prismaService: PrismaService) { }
+    constructor(private prismaService: PrismaService, private jwtService: JwtService) { }
     async signup(data: SignUpDTO) {
         const userAlreadyExists = await this.prismaService.user.findUnique({
             where: {
@@ -16,18 +18,45 @@ export class AuthService {
             throw new UnauthorizedException()
         }
 
-        const user = await this.prismaService.user.create({ data })
+        const hashedPassword = await bcrypt.hash(data.password, 10)
 
-        return { 
+        const user = await this.prismaService.user.create({
+            data: {
+                ...data,
+                password: hashedPassword,
+            }
+        })
+
+        return {
             id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
         };
     }
 
     async signin(data: SignInDTO) {
-        console.log(data);
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                email: data.email,
+            }
+        })
 
-        return 'signin';
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const passwordMatch = await bcrypt.compare(data.password, user.password);
+
+        if (!passwordMatch) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const accessToken = await this.jwtService.signAsync({
+            id: user.id,
+            name: user.name,
+            email: user.email
+        })
+
+        return { accessToken };
     }
 }
